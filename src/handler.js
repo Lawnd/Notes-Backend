324d00774c9725a4e8cb376f72812458640a3a40
Lawnd <nandahafidz24@gmail.com>
2025-01-14 22:17:13 +0700

    fix all

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,9 @@
-const { nanoid } = require("nanoid");
-const notes = require("./notes");
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
-  const { title, tags, body } = request.payload;
+  const { title = 'untitled', tags, body } = request.payload;
+
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
@@ -22,8 +23,8 @@ const addNoteHandler = (request, h) => {
 
   if (isSuccess) {
     const response = h.response({
-      status: "success",
-      message: "Catatan berhasil ditambahkan",
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
       data: {
         noteId: id,
       },
@@ -33,15 +34,15 @@ const addNoteHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: "fail",
-    message: "Catatan gagal ditambahkan",
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
   });
   response.code(500);
   return response;
 };
 
 const getAllNotesHandler = () => ({
-  status: "success",
+  status: 'success',
   data: {
     notes,
   },
@@ -49,11 +50,12 @@ const getAllNotesHandler = () => ({
 
 const getNoteByIdHandler = (request, h) => {
   const { id } = request.params;
+
   const note = notes.filter((n) => n.id === id)[0];
 
   if (note !== undefined) {
     return {
-      status: "success",
+      status: 'success',
       data: {
         note,
       },
@@ -61,8 +63,8 @@ const getNoteByIdHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: "fail",
-    message: "Catatan tidak ditemukan",
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
   });
   response.code(404);
   return response;
@@ -70,8 +72,10 @@ const getNoteByIdHandler = (request, h) => {
 
 const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
+
   const { title, tags, body } = request.payload;
   const updatedAt = new Date().toISOString();
+
   const index = notes.findIndex((note) => note.id === id);
 
   if (index !== -1) {
@@ -84,16 +88,16 @@ const editNoteByIdHandler = (request, h) => {
     };
 
     const response = h.response({
-      status: "success",
-      message: "Catatan berhasil diperbarui",
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: "fail",
-    message: "Gagal memperbarui catatan. Id tidak ditemukan",
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
   });
   response.code(404);
   return response;
@@ -101,20 +105,22 @@ const editNoteByIdHandler = (request, h) => {
 
 const deleteNoteByIdHandler = (request, h) => {
   const { id } = request.params;
+
   const index = notes.findIndex((note) => note.id === id);
+
   if (index !== -1) {
     notes.splice(index, 1);
     const response = h.response({
-      status: "success",
-      message: "Catatan berhasil dihapus",
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: "fail",
-    message: "Catatan gagal dihapus. Id tidak ditemukan",
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
   });
   response.code(404);
   return response;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
-  const server = Hapi.server({    
+  const server = Hapi.server({
     port: 5000,
-    // eslint-disable-next-line no-undef
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-            origin: ['*'],
-        },
+      cors: {
+        origin: ['*'],
+      },
     },
-});
+  });
 
   server.route(routes);
 
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
-//   console.log(`Join on Web http://notesapp-v1.dicodingacademy.com/`);
+  //   console.log(`Join on Web http://notesapp-v1.dicodingacademy.com/`);
 };
 
 init();
